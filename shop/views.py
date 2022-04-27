b97140aa37adcaf049d91a5a568b0e115af9e5b6
BJanbolot <soulwill.jb@gmail.com>
2022-04-27 16:01:48 +0600

    search

--- shop/views.py
+++ shop/views.py
@@ -1,19 +1,42 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
 
+from .helpers import product_list_filter_sort
 from .forms import ProductForm
 from .models import Category, Product
 
+
+def search_product(request):
+    category = None
+    categories = Category.objects.all()
+    products = None
+    search = request.GET.get('search')
+    if search:
+        products = Product.objects.filter(Q(name__icontains=search) | Q(description__icontains=search))
+    context = {
+        'products': products,
+        'categories': categories,
+        'category': category
+    }
+
+    return render(
+        request,
+        'product/product_list.html',
+        context
+    )
 
 def get_product_list(request, category_slug=None):
     """Функция вытаскивает продукты и если слаг приходит заполненым
     то фильтрует по слагу и в конце возвращаем контексты
     """
+    
     category = None
     categories = Category.objects.all()
-    products = Product.objects.filter(available=True)
+    products = Product.objects.filter(available=True).order_by('-created_at')
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
+    products = product_list_filter_sort(request, products, category_slug)
     context = {
         'products': products,
         'categories': categories,
